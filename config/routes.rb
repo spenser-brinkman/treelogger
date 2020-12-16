0c4dce0f5d24e37b242e963032217e86f9d58875
spenser6131 <brinkman.spenser@gmail.com>
2020-12-15 18:13:25 -0800

    basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :tree_surveys
+  # resources :properties
+  # resources :users
+  # resources :surveys
+  # resources :trees
+  root to: 'welcome#landing'
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post] 
 end
