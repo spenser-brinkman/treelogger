82e3c069b78c7d3beefc78a56b92992897221060
spenser6131 <brinkman.spenser@gmail.com>
2020-12-16 19:27:51 -0800

    setup login/signup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,44 @@
+class SessionsController < ApplicationController
+  
+  def welcome
+  end
+  
+  def new
+  end
+
+  def create
+    redirect_to root
+  end
+
+  def googleAuth
+    user = User.find_or_create_by(uid: access_token.uid) do |u|
+      u.name = access_token.extra.raw_info.given_name
+      u.email = access_token.extra.raw_info.email
+      u.password = SecureRandom.hex(12)
+    end
+    session[:user_id] = user.id
+
+    # The following chunk of code is not utilized but has been retained in case TreeLogger ends up implementing utilization of the Google API
+    # Access_token (defined in sessions/private) is used to authenticate requests made from the rails application to the google server
+    user.google_token = access_token.credentials.token
+    # Refresh_token to request new access_token
+    # Note: Refresh_token is only sent once during the first request
+    refresh_token = access_token.credentials.refresh_token
+    user.google_refresh_token = refresh_token if refresh_token.present?
+    user.save
+
+    redirect_to root_path
+  end
+
+  def destroy
+    reset_session
+    @current_user = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def access_token
+    request.env["omniauth.auth"]
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   
+  validates :name, presence: true, unless: ->(user){user.email.present?}
+  validates :email, presence: true, unless: ->(user){user.name.present?}
+  validates :email, uniqueness: true
+  validates_confirmation_of :password
+  
   has_secure_password
-
-  validates_presence_of :username, :password
-  validates :username, uniqueness: true
 
   has_many :properties
   has_many :surveys, through: :properties
   has_many :tree_surveys, through: :surveys
   has_many :trees, through: :tree_surveys
-
+  
 end
