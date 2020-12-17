1dea1c6e10c641c043f3c3cd85a330a761de1326
spenser6131 <brinkman.spenser@gmail.com>
2020-12-16 19:28:29 -0800

    moved helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,2 @@
 module ApplicationHelper
-  def current_user
-    @current_user ||= User.find(session[:user_id])
-  end
-
-  def logged_in?
-    !!session[:user_id]
-  end
-
-  def authentication
-    redirect '/login' if !logged_in?
-  end
-
-  def authorize(resource)
-    redirect back if resource.user != current_user
-  end
-
-  def today
-    DateTime.now.strftime("%Y-%m-%d")
-  end
 end
