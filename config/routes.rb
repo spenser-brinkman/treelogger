fef02dc867edfb6856f9a4f6619b67ab53285b7a
spenser6131 <brinkman.spenser@gmail.com>
2020-12-16 19:28:36 -0800

    add signup/login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # resources :tree_surveys
   # resources :properties
-  # resources :users
+  resources :users, except: :new
   # resources :surveys
   # resources :trees
-  root to: 'welcome#landing'
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post] 
+  root to: 'sessions#welcome'
+  get 'login', to: 'sessions#new'
+  get 'auth/:provider/callback', to: 'sessions#googleAuth', as: 'google_login'
+  get 'logout', to: 'sessions#destroy'
+  get 'auth/failure', to: redirect('/')
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  get 'users', to: 'users#index'
+
 end
