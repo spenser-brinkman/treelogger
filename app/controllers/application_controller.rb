a534da1a719a6fcf5b9e121415b8688a8d800b2f
spenser6131 <brinkman.spenser@gmail.com>
2020-12-16 19:27:10 -0800

    moved helpers over

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   
+  helper_method :current_user, :logged_in?, :today?
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def authentication
+    redirect '/login' if !logged_in?
+  end
+
+  def authorize(resource)
+    redirect back if resource.user != current_user
+  end
+
+  def today
+    DateTime.now.strftime("%Y-%m-%d")
+  end
+
 end
