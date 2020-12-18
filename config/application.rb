4810d99315aa15ccb07a1fed43a57c1c2eef74e2
spenser6131 <brinkman.spenser@gmail.com>
2020-12-17 17:10:40 -0800

    remove bulk routes

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,19 @@
 require_relative 'boot'
 
-require 'rails/all'
+# To clean up unused routes, this...
+# require 'rails/all'
+# Is replaced by this:
+require 'active_record/railtie'
+# require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
+require 'action_cable/engine'
+# require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,5 +28,8 @@ module TreeLogger2
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
+      html_tag
+    }
   end
 end
