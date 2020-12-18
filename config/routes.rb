4301536e6089b3a022f9654e21f98f6ea6915437
spenser6131 <brinkman.spenser@gmail.com>
2020-12-17 17:10:54 -0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # resources :tree_surveys
-  # resources :properties
+  resources :properties
   resources :users
   # resources :surveys
   # resources :trees
   root to: 'sessions#welcome'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
   get 'auth/:provider/callback', to: 'sessions#googleAuth', as: 'google_login'
   get 'logout', to: 'sessions#destroy'
   get 'auth/failure', to: redirect('/')
