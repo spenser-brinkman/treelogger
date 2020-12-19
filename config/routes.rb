83e7483dfd708053ccefcbd5e1a91f493b8e14d9
spenser6131 <brinkman.spenser@gmail.com>
2020-12-18 19:58:03 -0800

    add nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
-  # resources :tree_surveys
-  resources :properties
-  resources :users
-  # resources :surveys
-  # resources :trees
+
   root to: 'sessions#welcome'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'auth/:provider/callback', to: 'sessions#googleAuth', as: 'google_login'
   get 'logout', to: 'sessions#destroy'
   get 'auth/failure', to: redirect('/')
+  
+  resources :properties do
+    resources :surveys, shallow: true do
+      resources :trees, shallow: true
+    end
+  end
+  
+  resources :surveys, only: [:show, :edit, :update, :destroy]
+
+  get 'delete_property', to: 'properties#delete_property_confirmation', as: 'delete_property'
+  get 'choose_property_to_survey', to: 'surveys#choose_property_to_survey', as: 'start_new_survey'
+  # resources :tree_surveys
+  # resources :properties
+  resources :users
+  # resources :trees
 
 end
