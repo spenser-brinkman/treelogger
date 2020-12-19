bcbe438158cb7318753690dd1bcaee59901f55e3
spenser6131 <brinkman.spenser@gmail.com>
2020-12-18 19:30:02 -0800

    fleshed out survey controller

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,47 @@
 class SurveysController < ApplicationController
+
+  def index
+    @surveys = current_user.surveys
+  end
+
+  def show
+    get_survey
+  end
+
+  def new
+    @survey = Property.find_by(id: params[:property_id]).surveys.build
+  end
+
+  def create
+    @survey = Property.find_by(id: params[:property_id]).surveys.build(survey_params)
+    if @survey.save
+      redirect_to @survey
+    else
+      render new_property_survey_path(@survey.property)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def choose_property_to_survey
+    @properties = current_user.properties.all
+  end
+  
+  private
+  
+  def survey_params
+    params.require(:survey).permit(:date)
+  end
+
+  def get_survey
+    @survey = current_user.surveys.find_by(id: params[:id])
+  end
+
 end
