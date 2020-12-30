39f9080ba852defa46d3f16c977b1b288d21c19f
spenser6131 <brinkman.spenser@gmail.com>
2020-12-29 22:44:12 -0800

    change tree_surveys to inspections

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,8 +3,9 @@ class Property < ApplicationRecord
   validates :title, presence: true
   
   belongs_to :user
+  
   has_many :surveys
-  has_many :tree_surveys, through: :surveys
-  has_many :trees, through: :tree_surveys
+  has_many :trees
+  has_many :inspections, through: :surveys
 
 end
