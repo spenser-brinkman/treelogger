91f3e2ab4b9b52f46781c372676617ca4f655211
spenser6131 <brinkman.spenser@gmail.com>
2020-12-31 20:16:43 -0800

    basic inspections controller

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,7 +1,24 @@
 class InspectionsController < ApplicationController
 
   def new
+    @survey = Survey.find_by(id: params[:survey])
+    @inspection = Inspection.new
     @tree = Tree.find_by(id: params[:tree_id])
+  end
+
+  def create
+    @survey = Survey.find_by(id: params[:survey_id])
+    @inspection = @survey.inspections.build(inspection_params)
+    @inspection.tree = Tree.find_by(id: params[:tree_id])
+    if @inspection.save
+      redirect_to @survey
+    end
+  end
+
+  private
+
+  def inspection_params
+    params.require(:inspection).permit(:height, :dbh, :foliage, :comments)
   end
 
 end
