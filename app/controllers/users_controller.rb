5088fad4df97cf2473095734f1f9aa353819df5b
spenser6131 <brinkman.spenser@gmail.com>
2021-01-02 20:28:46 -0800

    more control over user accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   skip_before_action :authentication, only: [:new, :create]
-
+  helper_method :greet
+  
   def new
     @user = User.new
     @errors = @user.errors
@@ -19,7 +20,31 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find_by_id(params[:user_id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete_user_confirmation
+  end
+
+  def destroy
+    current_user.delete
+    byebug
+    redirect_to root_path
+  end
+
+  def greet
+    case Time.now.hour
+    when 4..11 then 'Good morning, '
+    when 12..17 then 'Good afternoon, '
+    when 18..23 then 'Good evening, '
+    else
+      'Hello, '
+    end
   end
 
   private
