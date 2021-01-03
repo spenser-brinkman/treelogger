84a0d5ea18d547c008a8b093ffc9edc812c4d88f
spenser6131 <brinkman.spenser@gmail.com>
2021-01-03 15:18:04 -0800

    stronger email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   
   validates :name, presence: true
-  validates :email, uniqueness: true, presence: true
   validates_confirmation_of :password
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create },
+              uniqueness: { case_sensitive: false },
+              length: { minimum: 4, maximum: 254 }
   
   has_secure_password
 
