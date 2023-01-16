e02c3a92d334135b80b4e25ea8e192ca237814fc
spenser-brinkman <brinkman.spenser@gmail.com>
2023-01-16 10:20:36 -0800

    Postgres -> MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -2,20 +2,16 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_232142) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "inspections", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2021_01_06_232142) do
+  create_table "inspections", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "survey_id"
     t.integer "tree_id"
     t.integer "height"
@@ -25,29 +21,29 @@ ActiveRecord::Schema.define(version: 2021_01_06_232142) do
     t.integer "user_id"
   end
 
-  create_table "properties", force: :cascade do |t|
+  create_table "properties", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
   end
 
-  create_table "species", force: :cascade do |t|
+  create_table "species", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
   end
 
-  create_table "surveys", force: :cascade do |t|
+  create_table "surveys", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "property_id"
     t.integer "user_id"
     t.date "date"
   end
 
-  create_table "trees", force: :cascade do |t|
+  create_table "trees", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "species_id"
     t.string "name"
     t.integer "user_id"
     t.integer "property_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
     t.string "google_token"
