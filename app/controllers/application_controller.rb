a59acb0f9639992740d38a52af86a482ae66b83e
spenser-brinkman <brinkman.spenser@gmail.com>
2023-03-10 14:01:45 -0800

    Change CSRF protection for easier local development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  
+  protect_from_forgery with: :null_session
+
   helper_method :current_user
   helper_method :logged_in?
   helper_method :today
