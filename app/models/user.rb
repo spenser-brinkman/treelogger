92345c68533178a097d84db55a3bf4282ea92ca5
spenser-brinkman <brinkman.spenser@gmail.com>
2023-03-10 14:00:35 -0800

    Lighten password constraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,22 @@
 class User < ApplicationRecord
-  
   validates :name, presence: true
   validate :password_complexity
   validates_confirmation_of :password
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: "is invalid." },
-              uniqueness: { case_sensitive: false },
-              length: { minimum: 4, maximum: 254 }
-  
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'is invalid.' },
+                    uniqueness: { case_sensitive: false },
+                    length: { minimum: 4, maximum: 254 }
+
   has_secure_password
 
   has_many :properties
   has_many :trees, through: :properties
   has_many :surveys, through: :properties
   has_many :inspections, through: :surveys
-  
-  def password_complexity
-    # return if password.blank? || password =~ /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,30}$/
-    return if password.blank? || password =~ /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-    errors.add :password, "does not meet requirements."
-  end
 
+  def password_complexity
+    # pages.nist.gov/800-63-3 encourages password length over complexity
+    return if password.blank? || password =~ /\A(?=.*\S).{8,256}\z/
+
+    errors.add :password, 'does not meet requirements.'
+  end
 end
