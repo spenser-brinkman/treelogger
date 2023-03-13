a0b1a8b9403c131e9d03a18aa62ebc49f7b9b92d
spenser-brinkman <brinkman.spenser@gmail.com>
2023-03-13 16:03:45 -0700

    Update Google sign in strategy

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-    prompt: 'select_account',
-    scope: 'email, profile, openid'
-  }
+  provider :google_oauth2,
+           Rails.application.credentials.GOOGLE_CLIENT_ID,
+           Rails.application.credentials.GOOGLE_CLIENT_SECRET, {
+             prompt: 'select_account',
+             scope: 'email, profile, openid'
+           }
 end
+
+# OmniAuth.config.allowed_request_methods = %i[get]
