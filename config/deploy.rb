54adb629119e1e25cc150cd44381b5dd4ee38e24
spenser-brinkman <brinkman.spenser@gmail.com>
2023-03-15 15:12:57 -0700

    Configure capistrano for production credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,10 +29,22 @@ set :branch, 'main'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/master.key'
+append :linked_files,
+       'config/database.yml',
+       'config/master.key',
+       'config/credentials/production.key',
+       'config/credentials/production.yml.enc'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/webpacker', 'public/system', 'vendor', 'storage'
+append :linked_dirs,
+       'log',
+       'tmp/pids',
+       'tmp/cache',
+       'tmp/sockets',
+       'tmp/webpacker',
+       'public/system',
+       'vendor',
+       'storage'
 
 # Default value for default_env is {}
 # set :default_env, { path: '/opt/ruby/bin:$PATH' }
